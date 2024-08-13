4dc47bd93207a508a3e0a96c39eb665b8030c5ba
yyhago <yhago.felipe.teles@gmail.com>
2024-08-13 18:34:11 -0300

    Created functions for blocks and conditions in hability

--- src/index.js
+++ src/index.js
@@ -18,15 +18,54 @@ async function rollDice(){
   return Math.floor(Math.random() * 6) + 1;
 }
 
-async function playRaceEngine(character1, character2){
+async function getRandomBlock(){
+  let random = Math.random();
+  let result;
 
-  for(let round = 1; round <= 5; round++)
-    console.log(`🚗 Rodada: ${round}`)
+  switch (true) {
+    case random < 0.33:
+      result = "RETA";
+      break;
+    case random < 0.66:
+      result = "CURVA";
+      break;
+    default:
+      result = "CONFRONTO";
+  }
+  return result;
+}
+
+async function playRaceEngine(character1, character2){
+  for(let round = 1; round <= 5; round++) {
+    console.log(`🚗 Rodada: ${round}`);
+    
+    let block = await getRandomBlock();
+    console.log(`Bloco: ${block}`);
+  }
+
+  let diceResults1 = await rollDice();
+  let diceResults2 = await rollDice();
+
+  let totalTestSkiils1 = 0
+  let totalTestSkiils2 = 0
+
+  if(block === "RETA"){
+    totalTestSkiils1 = diceResults1 + character1.VELOCIDADE
+    totalTestSkiils2 = diceResults2 + character2.VELOCIDADE
+  }
+  if(block === "CURVA"){
+    totalTestSkiils1 = diceResults1 + character1.MANOBRABILIDADE
+    totalTestSkiils2 = diceResults2 + character2.MANOBRABILIDADE
+  }
+  if(block === "CONFRONTO"){
+    let powerResults1 = diceResults1 + character1.PODER
+    let powerResults2 = diceResults2 + character2.PODER
+  }
+
 }
 
 (async function main(){
   console.log(`🏁🚥 A corrida entre ${player1.NOME} e ${player2.NOME} vai começar ... 🏁🚥\n`);
 
-  await playRaceEngine(player1, player2)
-
-})()
+  await playRaceEngine(player1, player2);
+})();
